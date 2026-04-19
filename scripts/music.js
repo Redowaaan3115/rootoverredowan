352faefbde18e025d416b3f29d68762ec5821ef9
rootoverredowan <redowaaan@protonmail.com>
2026-04-19 16:44:17 +0600

    Delete scripts directory

--- scripts/music.js
+++
@@ -1,5 +0,0 @@
-const audio = document.getElementById("audioPlayer");
-document.getElementById("uploadMusic").onchange = (e) => {
-    const file = e.target.files[0];
-    if (file) audio.src = URL.createObjectURL(file);
-};
